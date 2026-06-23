e4185522765717c6dda122307f73f974bb5d270f
Eliana Azcurra <azcurraely@gmail.com>
2026-06-23 15:40:54 -0300

    feat: performance optimizations and fix broken map listing images

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,8 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone'
   }),
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  image: {
+    domains: ['static.tokkobroker.com', 'muvpropiedades.com', 'images.unsplash.com']
+  }
 });
